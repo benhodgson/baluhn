13c9f58b9eb0a3ad81aaff8c1d946f4369a1092c
Tom Hendrikx <tom@fourdigits.nl>
2019-03-13 17:48:00 +0100

    Include the README in the package, so it can be displayed on pypi

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
-from distutils.core import setup
+from os import path
+from setuptools import setup
+
+
+def long_description():
+    this_directory = path.abspath(path.dirname(__file__))
+    with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+        return f.read()
+
 
 setup(
     name='baluhn',
@@ -6,6 +14,8 @@ setup(
     license='Public Domain',
     description='A base-agnostic implementation of the Luhn Algorithm for '
         'Python. Useful for generating and verifying check digits.',
+    long_description=long_description(),
+    long_description_content_type='text/markdown',
     package_dir = {'':'src'},
     py_modules=['baluhn'],
     url='http://github.com/benhodgson/baluhn',
